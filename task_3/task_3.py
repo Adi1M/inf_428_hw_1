2f000d26f288ce45aacbc2dd73933fdf04131264
Adil <97668941+Adi1M@users.noreply.github.com>
2024-11-26 12:29:36 +0500

    Changing the solution of task 3 after review

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -1,5 +1,12 @@
+import math
+
 def time_diff(time_from, time_to):
-    if time_from > time_to:
-        return (24 + time_to) - time_from
-    else:
-        return time_to - time_from
+    angle_from = 2 * math.pi * time_from / 24
+    angle_to = 2 * math.pi * time_to / 24
+    
+    angle_diff = abs(angle_to - angle_from)
+    
+    if angle_diff > math.pi:
+        angle_diff = 2 * math.pi - angle_diff
+    
+    return int(round(angle_diff * 24 / (2 * math.pi)))
